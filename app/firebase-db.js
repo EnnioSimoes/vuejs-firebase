1503fcb6040a843408d356f4560e8b6e46a23f33
Ennio Simões <ennio.simoes@outlook.com>
2016-08-06 18:13:50 -0300

    aula webpack-dev-server ok

---
+++ app/firebase-db.js
@@ -0,0 +1,9 @@
+var Firebase = require('firebase');
+var firebaseApp = Firebase.initializeApp({
+  apiKey: "AIzaSyDaWR8G1TMrDtGAQRHscbH833yV63TZzvg",
+  authDomain: "vuejs-firebase-4aeb4.firebaseapp.com",
+  databaseURL: "https://vuejs-firebase-4aeb4.firebaseio.com",
+  storageBucket: "vuejs-firebase-4aeb4.appspot.com",
+});
+
+module.exports = firebaseApp.database();
---
+++ app/rooms-create.component.js
@@ -0,0 +1,37 @@
+var rooms = [
+    {id: "001", name: "PHP", description: "Só fera do PHP"},
+    {id: "002", name: "Java", description: "Só fera do Java"},
+    {id: "003", name: "C#", description: "Só fera do C#"},
+    {id: "004", name: "C++", description: "Só fera do C++"},
+    {id: "005", name: "Javascript", description: "Só fera do Javascript"},
+    {id: "006", name: "VueJs", description: "Só fera do VueJs"},
+];
+
+var Vue = require('vue');
+var VueFire = require('vuefire');
+var db = require('./firebase-db');
+
+Vue.use(VueFire);
+
+module.exports = {
+    template: `
+        <ul>
+            <li v-for="o in rooms">
+                {{ o.name }}
+            </li>
+        </ul>
+    `,
+    firebase: {
+        rooms: db.ref('chat/rooms')
+    },
+    ready: function() {
+        var chatRef = db.ref('chat');
+        var roomsChildren = chatRef.child('rooms');
+        rooms.forEach(function(room) {
+            roomsChildren.child(room.id).set({
+                name: room.name,
+                description: room.description
+            });
+        });
+    }
+};
